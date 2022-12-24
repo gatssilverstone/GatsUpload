faa26c9c2306890110dd8c437e9c611e4e4eceb7
mahmudownariman <mahmudownariman01@gmail.com>
2022-12-24 15:49:02 +0300

    picturebox

--- sondeneme1/Program.cs
+++ sondeneme1/Program.cs
@@ -12,6 +12,8 @@ namespace sondeneme1
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            Console.WriteLine("Hello world");
         }
     }
 }
