f83480a90b93d9817d26438ab1d5beca0a078874
mahmudownariman <mahmudownariman01@gmail.com>
2022-12-24 16:35:56 +0300

    güncel

--- sondeneme1/Form1.Designer.cs
+++ sondeneme1/Form1.Designer.cs
@@ -28,20 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ayarlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.genelAyarlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lokalAyarlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bankalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dizaynlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hızlıTuşPaletToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kullanıcılarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.şifreDegiştirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.işlemlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.raporlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayarlarToolStripMenuItem,
+            this.işlemlerToolStripMenuItem,
+            this.raporlarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1348, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ayarlarToolStripMenuItem
+            // 
+            this.ayarlarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.genelAyarlarToolStripMenuItem,
+            this.lokalAyarlarToolStripMenuItem,
+            this.bankalarToolStripMenuItem,
+            this.dizaynlarToolStripMenuItem,
+            this.hızlıTuşPaletToolStripMenuItem,
+            this.kullanıcılarToolStripMenuItem,
+            this.şifreDegiştirToolStripMenuItem});
+            this.ayarlarToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ayarlarToolStripMenuItem.Name = "ayarlarToolStripMenuItem";
+            this.ayarlarToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.ayarlarToolStripMenuItem.Text = "Ayarlar";
+            // 
+            // genelAyarlarToolStripMenuItem
+            // 
+            this.genelAyarlarToolStripMenuItem.Name = "genelAyarlarToolStripMenuItem";
+            this.genelAyarlarToolStripMenuItem.Size = new System.Drawing.Size(195, 26);
+            this.genelAyarlarToolStripMenuItem.Text = "Genel Ayarlar ";
+            // 
+            // lokalAyarlarToolStripMenuItem
+            // 
+            this.lokalAyarlarToolStripMenuItem.Name = "lokalAyarlarToolStripMenuItem";
+            this.lokalAyarlarToolStripMenuItem.Size = new System.Drawing.Size(195, 26);
+            this.lokalAyarlarToolStripMenuItem.Text = "Lokal Ayarlar";
+            // 
+            // bankalarToolStripMenuItem
+            // 
+            this.bankalarToolStripMenuItem.Name = "bankalarToolStripMenuItem";
+            this.bankalarToolStripMenuItem.Size = new System.Drawing.Size(195, 26);
+            this.bankalarToolStripMenuItem.Text = "Bankalar";
+            // 
+            // dizaynlarToolStripMenuItem
+            // 
+            this.dizaynlarToolStripMenuItem.Name = "dizaynlarToolStripMenuItem";
+            this.dizaynlarToolStripMenuItem.Size = new System.Drawing.Size(195, 26);
+            this.dizaynlarToolStripMenuItem.Text = "Dizaynlar";
+            // 
+            // hızlıTuşPaletToolStripMenuItem
+            // 
+            this.hızlıTuşPaletToolStripMenuItem.Name = "hızlıTuşPaletToolStripMenuItem";
+            this.hızlıTuşPaletToolStripMenuItem.Size = new System.Drawing.Size(195, 26);
+            this.hızlıTuşPaletToolStripMenuItem.Text = "Hızlı Tuş Palet";
+            // 
+            // kullanıcılarToolStripMenuItem
+            // 
+            this.kullanıcılarToolStripMenuItem.Name = "kullanıcılarToolStripMenuItem";
+            this.kullanıcılarToolStripMenuItem.Size = new System.Drawing.Size(195, 26);
+            this.kullanıcılarToolStripMenuItem.Text = "Kullanıcılar";
+            // 
+            // şifreDegiştirToolStripMenuItem
+            // 
+            this.şifreDegiştirToolStripMenuItem.Name = "şifreDegiştirToolStripMenuItem";
+            this.şifreDegiştirToolStripMenuItem.Size = new System.Drawing.Size(195, 26);
+            this.şifreDegiştirToolStripMenuItem.Text = "Şifre Degiştir";
+            // 
+            // işlemlerToolStripMenuItem
+            // 
+            this.işlemlerToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.işlemlerToolStripMenuItem.Name = "işlemlerToolStripMenuItem";
+            this.işlemlerToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.işlemlerToolStripMenuItem.Text = "İşlemler";
+            // 
+            // raporlarToolStripMenuItem
+            // 
+            this.raporlarToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.raporlarToolStripMenuItem.Name = "raporlarToolStripMenuItem";
+            this.raporlarToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.raporlarToolStripMenuItem.Text = "Raporlar";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-2, 31);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1350, 693);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1342, 660);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1342, 657);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(971, 447);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Zenoyap";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem ayarlarToolStripMenuItem;
+        private ToolStripMenuItem genelAyarlarToolStripMenuItem;
+        private ToolStripMenuItem lokalAyarlarToolStripMenuItem;
+        private ToolStripMenuItem bankalarToolStripMenuItem;
+        private ToolStripMenuItem dizaynlarToolStripMenuItem;
+        private ToolStripMenuItem hızlıTuşPaletToolStripMenuItem;
+        private ToolStripMenuItem kullanıcılarToolStripMenuItem;
+        private ToolStripMenuItem şifreDegiştirToolStripMenuItem;
+        private ToolStripMenuItem işlemlerToolStripMenuItem;
+        private ToolStripMenuItem raporlarToolStripMenuItem;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
